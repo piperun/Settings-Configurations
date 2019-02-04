91f74a825adfea59bc42da58bea16d2fab8d8350
piperun <nevadadirren@gmail.com>
2019-02-04 13:36:56 +0100

    Create ansible-template.sh

---
+++ tools/ansible-template.sh
@@ -0,0 +1,5 @@
+
+
+mkdir -p group_vars host_vars library module_utils filter_plugins
+mkdir -p roles/common/{tasks,handlers,templates,files,vars,defaults,meta,library,module_utils,lookup_plugins}
+touch production staging site.yml roles/common/{tasks,handlers,templates,files,vars,defaults,meta}/main.yml
